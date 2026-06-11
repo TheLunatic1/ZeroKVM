4473a4f4d37f2345518139c09a862aec6706b9fa
Salman Toha <ishrak1846@gmail.com>
2026-06-11 16:38:03 +0600

    v4

--- frontend-pwa/dev-dist/sw.js
+++ frontend-pwa/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.14c57f8hg08"
+    "revision": "0.lvetvh25deg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
